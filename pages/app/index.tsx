93a35059c6534bb5f42e21fba4371d0f766af828
xencodes <xencodes@icloud.com>
2021-11-23 20:26:39 +0530

    chore: update app file

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import { Box } from '@chakra-ui/layout'
-import Layout from '../../components/layout'
 
-const App: React.FC = () => {
+const Dashboard: React.FC = () => {
   return <Box></Box>
 }
 
-export default App
+export default Dashboard
