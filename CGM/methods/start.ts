9e47e9dfc767ff997b5e14c274af7125d816307d
xencodes <xencodes@icloud.com>
2021-11-23 19:22:46 +0530

    chore: working start command

--- CGM/methods/start.ts
+++ CGM/methods/start.ts
@@ -1,7 +1,24 @@
 import { Composer } from 'grammy'
+import shortid from 'shortid'
+
+import { prisma } from '../../providers/prisma'
 
 const start = new Composer()
 
-start.command('start', (ctx) => {})
+start.command('start', async (ctx) => {
+  let user = await prisma.user.findUnique({ where: { id: ctx.from?.id } })
+
+  if (user === null) {
+    user = await prisma.user.create({
+      data: {
+        id: ctx.from?.id as number,
+        name: ctx.from?.first_name as string,
+        token: shortid.generate(),
+      },
+    })
+  }
+
+  ctx.reply('Your token is: ' + user.token)
+})
 
 export { start }
